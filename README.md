93ea4b01fa4a97a9c6c2b0113dc45f6baf92b12b
fsickle <wanghyscu@163.com>
2018-08-06 22:57:08 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 <h2>尝试模拟登陆boss直聘网站</h2>
+<p>从 boss直聘登陆页面可得到验证码的 key,通过它下载得到验证码,识别后输入验证码值</p></br>
+<p>(尝试使用 tesserocr 识别验证码失败后，采用人脸识别</p><br>
+<p>输入得到验证码后，会发送验证码到手机。输入之后即可完成登陆。结果如下：</P>
+<img src='result/result1.png'>
+<p>爬虫会对成都的python岗位进行爬取，到爬取到一定时间会失败。结果如下：</p>
+<img src='result/result2.png'>
+<p>将结果存储到 mongodb,如图：</P>
+<img src='result/result3.png'>
--- login.py
+++ login.py
@@ -6,13 +6,14 @@ import time
 import random
 import pymongo
 import base64
+from urllib.parse import urlencode
 
 
 class Login():
     def __init__(self, proxy=None):
         self.headers = {
             'Proxy-Authorization': self.get_proxy(),
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+            'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
             'referer': 'https://www.zhipin.com /',
         }
         self.session = requests.Session()
@@ -20,8 +21,8 @@ class Login():
         self.db = self.client['jobs']
 
     def get_proxy(self):
-        proxyUser = 'H74274906A74PP2D'
-        proxyPass = 'EA516E778B9E0A75'
+        proxyUser = input('proxy id:')
+        proxyPass = input('proxy pass:')
         end = proxyUser + ":" + proxyPass
         a = base64.b64encode(end.encode('utf-8')).decode('utf-8')
         proxy = "Basic " + a
@@ -41,7 +42,7 @@ class Login():
     def download_img(self, key):
         img_url = 'https://login.zhipin.com/captcha/?randomKey=' + key
         headers = self.headers.copy()
-        headers['refer'] = 'https://login.zhipin.com/?ka=header-login'
+        headers['referer'] = 'https://login.zhipin.com/?ka=header-login'
         response = self.session.get(img_url, headers=headers)
         print('开始下载图片')
         file_path = '{0}/captcha.{1}'.format(os.getcwd(), 'png')
@@ -54,19 +55,19 @@ class Login():
         print('你输入的验证码是：%s'% captcha)
         return captcha
 
-    def send_sms(self, key, value):
+    def send_sms(self, phone, key, value):
         url = 'https://login.zhipin.com/registe/sendSms.json'
         form_data = {
             'pk': 'cpc_user_sign_up',
             'regionCode': '+86',
-            'phone': '18328592041',
+            'phone': phone,
             'captcha': value,
             'randomKey': key,
             'phoneCode': '',
             'smsType': '1',
         }
         headers = self.headers.copy()
-        headers['refer'] = 'https://login.zhipin.com/?ka=header-login'
+        headers['referer'] = 'https://login.zhipin.com/?ka=header-login'
         headers['x-requested-with'] = 'XMLHttpRequest'
         response = self.session.post(url, headers=headers, data=form_data)
         print(response.text)
@@ -90,7 +91,7 @@ class Login():
             'smsType': '1',
         }
         headers = self.headers.copy()
-        headers['refer'] = 'https://login.zhipin.com/?ka=header-login'
+        headers['referer'] = 'https://login.zhipin.com/?ka=header-login'
         headers['x-requested-with'] = 'XMLHttpRequest'
         response = self.session.post(url, headers=headers, data=form_data)
         print(response.text)
@@ -109,24 +110,38 @@ class Login():
             'ka': 'page-' + str(n),
         }
         headers = self.headers.copy()
-        #headers['refer'] = 'https://www.zhipin.com/c101270100/h_101270100/?query=python'
+        if n != 1:
+            refer_query = {
+                'query': 'python',
+                'page': str(n-1),
+                'ka': 'page-' + str(n-1),
+            }
+            headers['referer'] = index_url + '?' + urlencode(refer_query)
         session = self.session
         response = session.get(index_url, headers=headers, params=query)
         return response.text
 
-    def parse(self, content):
+    def parse(self, content, n):
         soup = BeautifulSoup(content, 'lxml')
         hrefs = soup.select('#main > div > div.job-list > ul > li > div > div.info-primary > h3 > a')
+
         for href in hrefs:
             job_url = 'https://www.zhipin.com' + href['href']
-            time.sleep(random.random())
-            item = self.parse_job(job_url)
+            print(job_url)
+            time.sleep(random.random()*5)
+            item = self.parse_job(job_url, n)
             self.save_to_mongo(item)
 
-
-    def parse_job(self, job_url):
+    def parse_job(self, job_url, n):
         session = self.session
         headers = self.headers.copy()
+        refer_query = {
+            'query': 'python',
+            'page': str(n),
+            'ka': 'page-' + str(n),
+        }
+        refer_url = 'https://www.zhipin.com/c101270100/h_101270100?' + urlencode(refer_query)
+        headers['referer'] = refer_url
         response = session.get(job_url, headers=headers)
         content = pq(response.text)
         item = dict()
@@ -136,12 +151,12 @@ class Login():
         item['describe'] = content('#main div.job-box div div.job-detail div.detail-content div:nth-child(1) div.text').text()
         item['team'] = content('#main div.job-box div div.job-detail div.detail-content div:nth-child(2) div.job-tags').text()
         item['company'] = content('#main div.job-box div div.job-detail div.detail-content div.job-sec.company-info div').text()
-        item['url'] = url
+        item['url'] = job_url
         item['location'] = content('#main div.job-box div div.job-detail div.detail-content div:nth-child(6) div div.location-address').text()
         return item
 
     def save_to_mongo(self, item):
-        if self.db['boss_jobs'].insert(item):
+        if self.db['boss_jobs'].insert_one(item):
             print('存储到MongoDB', item)
             return True
         return False
@@ -152,9 +167,10 @@ if __name__ == '__main__':
     html = s.start_requests()
     key = s.get_key(html)
     captcha = s.download_img(key)
-    code = s.send_sms(key, captcha)
+    phone = input('你的手机号码是：')
+    code = s.send_sms(phone, key, captcha)
     result = s.login_in(key, captcha, code)
     if result:
         for i in range(1, 50):
             content = s.parse_index(i)
-            s.parse(content)
+            s.parse(content, i)
